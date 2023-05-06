69f9c0cfa9213f3b221a10f63bb135c2ed288cf6
Sara Marcos Cornejo <102854613+saramarcoss@users.noreply.github.com>
2023-05-06 18:46:52 +0200

    Add files via upload

--- blockchain.rb
+++ blockchain.rb
@@ -1,89 +1,82 @@
-###########################
-#	Improved version of "Build your own blockchain from scratch in 20 lines of Ruby!"	
-#		from https://github.com/openblockchains/awesome-blockchains/tree/master/blockchain.rb
-#  
-#   and inspired by
-#     Let's Build the Tiniest Blockchain In Less Than 50 Lines of Python by Gerald Nash
-#     see https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
-#
-#	Now, Blockchain with prompt transactions, transactions counter for each block, 
-#											 ledger, proof of work and dynamic variable name. 
-#
-#	This Blockchain can be set as a loop for infinite using of the Blockchain.
-#
-#
-#  to run use:
-#    $ ruby ./blockchain.rb
-#
-#
-#
-
-require 'digest'    							# For hash checksum digest function SHA256
-require 'pp'        							# For pp => pretty printer
-# require 'pry'                     # For on the fly debugging
-require_relative 'block'					# class Block
-require_relative 'transaction'		# method Transactions
+require 'digest'
+require 'pp'
+require_relative 'block'
+require_relative 'transaction'
 
 LEDGER = []
 
-#####
-## Blockchain building, one block at a time.
-##  This will create a first block with fake transactions
-## 	and then prompt user for transactions informations and set it in a new block.
-## 	
-## Each block can take multiple transaction
-## 	when a user has finish to add transaction, 
-##  the block is added to the blockchain and writen in the ledger
-
-
 def create_first_block
-	i = 0
-	instance_variable_set( "@b#{i}", 
-												 Block.first( 
-													{ from: "Dutchgrown", to: "Vincent", what: "Tulip Bloemendaal Sunset", qty: 10 },
-													{ from: "Keukenhof", to: "Anne", what: "Tulip Semper Augustus", qty: 7 } )
-											 )
-	LEDGER << @b0
-	pp @b0
-	p "============================"
-	add_block
+  i = 0
+  instance_variable_set("@b#{i}", Block.first(
+    { from: "Dutchgrown", to: "Vincent", what: "Tulip Bloemendaal Sunset", qty: 10 },
+    { from: "Keukenhof", to: "Anne", what: "Tulip Semper Augustus", qty: 7 }
+  ))
+  LEDGER << @b0
+  pp @b0
+  p "============================"
+  menu
 end
-	
-	
-	
+
 def add_block
 	i = 1
 	loop do
-		instance_variable_set("@b#{i}", Block.next( (instance_variable_get("@b#{i-1}")), get_transactions_data))
-		LEDGER << instance_variable_get("@b#{i}")
-		p "============================"
-		pp instance_variable_get("@b#{i}")
-		p "============================"
-		i += 1
+	  transactions = get_transactions_data
+	  instance_variable_set("@b#{i}", Block.next((instance_variable_get("@b#{i - 1}")), transactions))
+	  LEDGER << instance_variable_get("@b#{i}")
+	  p "============================"
+	  pp instance_variable_get("@b#{i}")
+	  p "============================"
+	  i += 1
 	end
+  end
+
+def list_all_blocks
+  LEDGER.each do |block|
+    pp block
+    puts "============================"
+  end
 end
 
-def launcher
-	puts "==========================="
-	puts ""
-	puts "Welcome to Simple Blockchain In Ruby !"
-	puts ""
-	sleep 1.5
-	puts "This program was created by Anthony Amar for and educationnal purpose"
-	puts ""
-	sleep 1.5
-	puts "Wait for the genesis (the first block of the blockchain)"
-	puts ""
-	for i in 1..10
-		print "."
-		sleep 0.5
-		break if i == 10
-	end
-	puts "" 
-	puts "" 
-	puts "==========================="
-	create_first_block
+def exit_program
+  exit
 end
 
+def invalid_choice
+  puts "Invalid choice! Please try again."
+end
 
-launcher
+def menu
+	puts "Select an option:"
+	puts "1. Add a transaction"
+	puts "2. List all blocks"
+	puts "3. List all transactions"
+	puts "4. Save transactions to file"
+	puts "5. Exit"
+  
+	choice = gets.chomp.to_i
+  
+	case choice
+	when 1
+	  add_transaction
+	when 2
+	  list_all_blocks
+	when 3
+	  list_all_transactions
+	when 4
+	  save_transactions_to_file
+	when 5
+	  exit_program
+	else
+	  invalid_choice
+	end
+  
+	menu
+  end
+  
+
+def run
+  create_first_block
+  menu
+end
+
+run
--- transaction.rb
+++ transaction.rb
@@ -1,35 +1,92 @@
 def get_transactions_data
-
-	transactions_block ||= []
-	blank_transaction = Hash[from: "", to: "",
-													 what: "", qty: ""]
-	loop do
-		puts "" 
-		puts "Enter your name for the new transaction"
-		from = gets.chomp
-		puts "" 
-		puts "What do you want to send ?"
-		what = gets.chomp
-		puts "" 
-		puts "How much quantity ?"
-		qty  = gets.chomp
-		puts "" 
-		puts "Who do you want to send it to ?"
-		to 	 = gets.chomp
-
-		transaction = Hash[from: "#{from}", to: "#{to}", 
-											 what: "#{what}", qty: "#{qty}"]
-		transactions_block << transaction
-
-		puts "" 
-		puts "Do you want to make another transaction for this block ? (Y/n)"
-		new_transaction = gets.chomp.downcase
-
-		if new_transaction == "y"
-			self
-		else
-			return transactions_block
-			break
-		end
+	def valid_number?(input)
+	  input.match?(/^\d+(\.\d+)?$/)
 	end
-end
+  
+	transactions_block ||= []
+	blank_transaction = Hash[from: "", to: "", what: "", qty: ""]
+	loop do
+	  puts ""
+	  puts "Enter your name for the new transaction"
+	  from = gets.chomp
+	  while from.empty?
+		puts "This value cannot be empty"
+		print "Enter the name for the transaction: "
+		from = gets.chomp
+	  end
+  
+	  puts ""
+	  puts "What do you want to send ?"
+	  what = gets.chomp
+	  while what.empty?
+		puts "This value cannot be empty."
+		print "Enter what you want to send: "
+		what = gets.chomp
+	  end
+  
+	  puts ""
+	  puts "How much quantity ?"
+	  qty = gets.chomp
+  
+	  while qty.empty? || !valid_number?(qty)
+		if qty.empty?
+		  puts "This value cannot be empty."
+		else
+		  puts "This value has to be a number."
+		end
+		print "Enter a number "
+		qty = gets.chomp
+	  end
+  
+	  puts ""
+	  puts "Who do you want to send it to ?"
+	  to = gets.chomp
+	  while to.empty?
+		puts "This value cannot be empty."
+		print "Enter the destination: "
+		to = gets.chomp
+	  end
+  
+	  transaction = Hash[from: "#{from}", to: "#{to}", what: "#{what}", qty: "#{qty}"]
+	  transactions_block << transaction
+  
+	  puts ""
+	  puts "Do you want to make another transaction for this block ? (Y/n)"
+	  new_transaction = gets.chomp.downcase
+  
+	  if new_transaction == "n"
+		return transactions_block
+	  end
+	end
+  end
+  
+  def add_transaction
+	  latest_block = LEDGER.last
+	  latest_block_hash = latest_block.hash
+	  new_transactions = get_transactions_data
+	  new_block = Block.next(latest_block, new_transactions)
+	  LEDGER << new_block
+	  puts "New block created with #{new_block.transactions.size} transactions."
+	end
+	
+  
+  def list_all_transactions
+	LEDGER.each do |block|
+	  block.transactions.each do |transaction|
+		puts "From: #{transaction[:from]}, To: #{transaction[:to]}, What: #{transaction[:what]}, Qty: #{transaction[:qty]}"
+	  end
+	end
+  end
+  
+  def save_transactions_to_file
+	  puts "Enter file name to save transactions to (without .txt extension): "
+	  filename = gets.chomp
+	  File.open("#{filename}.txt", "w") do |file|
+		LEDGER.each do |block|
+		  block.transactions.each do |transaction|
+			file.puts "From: #{transaction[:from]}, To: #{transaction[:to]}, What: #{transaction[:what]}, Qty: #{transaction[:qty]}"
+		  end
+		end
+	  end
+	  puts "Transactions saved to #{filename}.txt"
+	end
